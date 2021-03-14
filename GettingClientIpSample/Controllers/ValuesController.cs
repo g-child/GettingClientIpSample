d95923b9b7e598f8f66f0a4a07d65fb9c6bd2fe8
kanekoh <kaneko.h.ai@gmail.com>
2021-03-14 18:14:24 +0900

    Add project files.

---
+++ GettingClientIpSample/Controllers/ValuesController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace GettingClientIpSample.Controllers
+{
+    [Route("")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly ILogger<ValuesController> _logger;
+
+        public ValuesController(ILogger<ValuesController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            var values = HttpContext.Request.Headers.Select(h => $"{h.Key} : {h.Value}").ToList();
+            values.Add($"remoteIpAddress: {HttpContext.Connection.RemoteIpAddress}");
+            return values;
+        }
+    }
+}
